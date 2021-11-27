2f42e4e39b5799b4466b52b6d265fe6122127607
sonchaegeon <thscorjs1002@naver.com>
2021-11-27 16:19:23 +0900

    ⚡️ :: chat entity

---
+++ src/entity/chat/chat.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Room } from '../room/room.entity';
+import { User } from '../user/user.entity';
+
+@Entity()
+export class Chat {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ length: 150, nullable: false })
+  message: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @ManyToOne(() => Room, (room) => room.chats)
+  @JoinColumn({ name: 'room_id' })
+  room: Room;
+
+  @ManyToOne(() => User, (user) => user.chats)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+}
