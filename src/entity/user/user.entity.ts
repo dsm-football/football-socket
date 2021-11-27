927e58b1e6a3a16f24558308d2bf804a4a5dd3f7
sonchaegeon <thscorjs1002@naver.com>
2021-11-27 16:19:30 +0900

    ⚡️ :: user entity

---
+++ src/entity/user/user.entity.ts
@@ -0,0 +1,39 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Chat } from '../chat/chat.entity';
+import { Room } from '../room/room.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ length: 80, nullable: false })
+  email: string;
+
+  @Column({ length: 16, nullable: false })
+  name: string;
+
+  @Column({ length: 150, nullable: false })
+  profile: string;
+
+  @Column()
+  age: number;
+
+  @Column({ type: 'bit', nullable: false })
+  is_pro: boolean;
+
+  @Column({ length: 200 })
+  bio: string;
+
+  @Column()
+  club_nack_num: number;
+
+  @OneToMany(() => Room, (room) => room.host_user)
+  host_rooms: Room[];
+
+  @OneToMany(() => Room, (room) => room.user)
+  rooms: Room[];
+
+  @OneToMany(() => Chat, (chat) => chat.user)
+  chats: Chat[];
+}
